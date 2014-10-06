812c44129428bc7c43a4ddcd90ca3d0b747cea2b
David Forck <dgforck1@cougars.ccis.edu>
2014-10-06 09:10:03 -0500

    updates

--- Chess.h
+++ Chess.h
@@ -1,12 +1,15 @@
 #ifndef CHESS_H
 #define CHESS_H
 
+#include<iostream>
+#include<vector>
+
 class Piece
 {
 public:
     //constructors
     Piece() {}; 
-    Piece(int r, int f, int p, std::string t) :
+    Piece(int p, std::string t, int r, int f) :
         rank(r), file(f), player(p), type(t)
     {};
 
@@ -27,6 +30,31 @@ private:
 
 
 
+class Board
+{
+public:
+    Board() //default constructor
+    {
+        initializePieces();
+        initializeBoard();
+    }
+
+    void initializePieces();
+    void initializeBoard();
+    void updateBoard();
+    Piece& getPiece(int r, int f);
+    int getPieceIndex(int r, int f);
+    bool checkMove(int i, int sourceR, int sourceF,
+                   int destR, int destF) const;
+    void movePiece(int i, int r, int f);
+    void capturePiece(int r, int f);
+    void printBoard() const;
+    
+private:
+    char board[8][8];
+    std::vector< Piece > p;
+    
+};
 
 
 #endif
