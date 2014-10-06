d1da19c5b1e8e007d41267e4e974e03deba11c81
David Forck <dgforck1@cougars.ccis.edu>
2014-10-06 09:10:30 -0500

    updates

--- main.cpp
+++ main.cpp
@@ -3,12 +3,30 @@
 #include <string>
 
 bool test_piece();
+bool test_board();
 
 int main()
 {
     //test cases
     std::cout << "Chess Test cases: " << std::endl;
-    test_piece();
+
+    if(test_piece())
+    {
+        std::cout << "Piece Tests passed" << std::endl;
+    }
+    else
+    {
+        std::cout << "Piece Tests failed" << std::endl;
+    }
+
+    if(test_board())
+    {
+        std::cout << "Board Tests passed" << std::endl;
+    }
+    else
+    {
+        std::cout << "Board Tests failed" << std::endl;
+    }
     
     return 0;
 }
@@ -17,44 +35,49 @@ int main()
 bool test_piece()
 {
     bool pass = true;
-    Piece p();
+    
+    Piece *p = new Piece();
     std::cout << "Piece empty constructor passed" << std::endl;
 
+    delete p;
     
-    Piece p2(0, 1, 2, "P");
+    Piece p2(0, "P", 1, 2);
     std::cout << "Piece constructor passed" << std::endl;
 
     
     int test = p2.getRank();
-    if(test == 0)
+    if(test == 1)
     {
         std::cout << "Piece getRank passed" << std::endl;
     }
     else
     {
         std::cout << "Piece getRank failed" << std::endl;
+        pass = false;
     }
 
     
     test = p2.getFile();
-    if(test == 1)
+    if(test == 2)
     {
         std::cout << "Piece getFile passed" << std::endl;
     }
     else
     {
         std::cout << "Piece getFile failed" << std::endl;
+        pass = false;
     }
 
     
     test = p2.getPlayer();
-    if(test == 2)
+    if(test == 0)
     {
         std::cout << "Piece getPlayer passed" << std::endl;
     }
     else
     {
         std::cout << "Piece getPlayer failed" << std::endl;
+        pass = false;
     }
 
     
@@ -66,7 +89,18 @@ bool test_piece()
     else
     {
         std::cout << "Piece getType failed" << std::endl;
+        pass = false;
     }
 
+    return pass;
+}
+
+bool test_board()
+{
+    bool pass = true;
+
+    Board *b = new Board();
+    std::cout << "Board default constructor passed" << std::endl;
+    
     return pass;
 }
