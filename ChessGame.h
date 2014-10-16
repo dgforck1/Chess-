f5475a76b88efc57b93de98b3c1377751cd8adc3
Michael Thomas <12mgthomas@gmail.com>
2014-10-16 17:04:07 -0500

    Started adding classes to ChessGame.h that will be used by the server

--- ChessGame.h
+++ ChessGame.h
@@ -18,10 +18,45 @@
 // End Client States
 //////////////////////////////////////////////////////////////////////////////
 
-
+// random ass fucntion
 void ChessMain();
 
+//////////////////////////////////////////////////////////////////////////////
+// Class Definitions
+//////////////////////////////////////////////////////////////////////////////
 
+class Client
+{
+public:    
+    Client(TCPsocket s=NULL, std::string n="",
+           bool i=false, bool a=false)
+        : sock(s), name(n), ingame(i), active(a)
+    {}
+    
+	TCPsocket sock;
+	std::string name;
+    bool active;
+};
+
+class Game
+{
+public:
+    Game(Client & p1)
+    {
+        players.push_back(p1);
+    }
+
+    void addSpectator (Client & s);
+    // add spectator
+    // add player
+    // the constructor will always need a player, that will be player 1
+    
+private:
+    //Client player1;
+    //Client player2;
+    std::vector<Client> players;
+    std::vector<Client> spectators;
+}
 
 
 
